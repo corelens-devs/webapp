a45ded46b685b2c145077b737f79589189a5a527
shriyatiwari17102000 <112067667+shriyatiwari17102000@users.noreply.github.com>
2024-05-07 20:22:13 +0530

    code commited

--- src/Components/FAQ/Faq.jsx
+++ src/Components/FAQ/Faq.jsx
@@ -11,33 +11,45 @@ const Faq = () => {
 
     <Accordion>
       <Accordion.Item eventKey="0" className={classes.accord}>
-        <Accordion.Header className={classes.head}>Korem ipsum dolor sit amet, consectetur adipiscing elit.</Accordion.Header>
+        <Accordion.Header className={classes.head}>How long does the recorded footage stay stored on the system?</Accordion.Header>
         <Accordion.Body className={classes.para}>
-        Borem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus
+        Our system typically retains recorded footage for 12 to 15 days, allowing you to review past events with ease.
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="1" className={classes.accord}>
-        <Accordion.Header className={classes.head}>Korem ipsum dolor sit amet, consectetur adipiscing elit.</Accordion.Header>
+        <Accordion.Header className={classes.head}>How do I extend the range of the wireless cameras?</Accordion.Header>
         <Accordion.Body className={classes.para}>
-        Borem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus
+        To extend the range of our wireless cameras, you can use Wi-Fi extenders strategically placed within your property. Download our app to buy wifi extenders.
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="2" className={classes.accord}>
-        <Accordion.Header className={classes.head}>Korem ipsum dolor sit amet, consectetur adipiscing elit.</Accordion.Header>
+        <Accordion.Header className={classes.head}>Do you offer professional installation services?</Accordion.Header>
         <Accordion.Body className={classes.para}>
-        Borem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus
+        No, we don’t provide professional installation services yet , but we assist with any help required through our toll free number 1800-313-4207.
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="3" className={classes.accord}>
-        <Accordion.Header className={classes.head}>Korem ipsum dolor sit amet, consectetur adipiscing elit.</Accordion.Header>
+        <Accordion.Header className={classes.head}>Are there any limitations on the number of cameras I can install in a single system?</Accordion.Header>
         <Accordion.Body className={classes.para}>
-        Borem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus
+        There are no strict limitations on the number of cameras you can install in our system. You can scale up your surveillance network according to your needs.
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="4" className={classes.accord}>
-        <Accordion.Header className={classes.head}>Korem ipsum dolor sit amet, consectetur adipiscing elit.</Accordion.Header>
+        <Accordion.Header className={classes.head}>Are your cameras compatible with voice assistants like Amazon Alexa or Google Assistant?</Accordion.Header>
         <Accordion.Body className={classes.para}>
-        Borem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus
+        Yes, our cameras are compatible with popular voice assistants such as Amazon Alexa and Google Assistant, allowing for convenient voice control and integration with smart home ecosystems.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="5" className={classes.accord}>
+        <Accordion.Header className={classes.head}>What measures do you take to ensure the privacy of my residence while under surveillance?</Accordion.Header>
+        <Accordion.Body className={classes.para}>
+        We prioritize your privacy by implementing robust security measures, including protocol to keep your personal information private like your home address and number. Moreover we differentiate between an indoor and outdoor sensor to provide extra layer of security to indoor ones and additional Specific department to avoid any leak of privacy. Your privacy is of utmost importance to us.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="6" className={classes.accord}>
+        <Accordion.Header className={classes.head}>What happens in case of theft, break-in, or emergency?</Accordion.Header>
+        <Accordion.Body className={classes.para}>
+        In the event of theft, break-in, or emergency, our surveillance system provides real-time alerts on your smartphone and if you have the surveillance turned on we take immediate action. Additionally, we inform local police station and your nearest neighbours through call. You can provide 10 contacts of friends or neighbours in the emergency contacts section of our app.
         </Accordion.Body>
       </Accordion.Item>
     </Accordion>
--- src/Components/Products/ProductSlider.jsx
+++ src/Components/Products/ProductSlider.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay } from 'swiper/modules';
@@ -10,8 +10,33 @@ import Product from './Product';
 import Heading from '../Heading/Heading';
 import classes from "./Product.module.css"
 const ProductSlider = () => {
+    const gradients = {
+        p1: 'linear-gradient(180deg, #FCB48C 0%, #FFEADF 100%)',
+        p2: 'linear-gradient(180deg, #6FDBF9 0%, rgba(207, 245, 255, 0.69) 100%)',
+        p3: 'linear-gradient(180deg, #E8EB8C 0%, #FEFFEE 100%)',
+        p4: 'linear-gradient(180deg, #FF9CA9 0%, rgba(255, 181, 191, 0.48) 100%)',
+      };
+
+      const [showAllProducts, setShowAllProducts] = useState(false);
+
+      const toggleProductsView = () => {
+        setShowAllProducts(!showAllProducts);
+    };
+
+      const products = [
+        { id: 1, name: 'Product 1', cls: classes.p1, gradientColors: gradients.p1 },
+        { id: 2, name: 'Product 2', cls: classes.p2, gradientColors: gradients.p2 },
+        { id: 3, name: 'Product 3', cls: classes.p3, gradientColors: gradients.p3 },
+        { id: 4, name: 'Product 4', cls: classes.p4, gradientColors: gradients.p4 },
+        { id: 5, name: 'Product 1', cls: classes.p1, gradientColors: gradients.p1 },
+        { id: 6, name: 'Product 2', cls: classes.p2, gradientColors: gradients.p2 },
+        { id: 7, name: 'Product 3', cls: classes.p3, gradientColors: gradients.p3 },
+        { id: 8, name: 'Product 4', cls: classes.p4, gradientColors: gradients.p4 },
+        // Add more products as needed
+    ];
+    
     return (
-        <div  id='product'>
+        <div  id='product' className={`${classes.productContainer} ${showAllProducts ? classes.showAllProducts : ''}`}>
         <Heading heading="Our Products" cls={classes.div_head}/>
         <Swiper
         pagination={{
@@ -43,19 +68,124 @@ const ProductSlider = () => {
 
 
             spaceBetween={30}
-            // slidesPerView={4}
         >
-            <SwiperSlide><Product/></SwiperSlide>
-            <SwiperSlide><Product cls={classes.p2}/></SwiperSlide>
-            <SwiperSlide><Product cls={classes.p4}/></SwiperSlide>
-            <SwiperSlide><Product cls={classes.p3}/></SwiperSlide>
-            <SwiperSlide><Product cls={classes.p4}/></SwiperSlide>
-            <SwiperSlide><Product cls={classes.p1}/></SwiperSlide>
+
+
+      {[...Array(10)].map((_, index) => (
+        <SwiperSlide key={index}>
+          <Product cls={classes[`p${(index % 4) + 1}`]} gradientColors={gradients[`p${(index % 4) + 1}`]} />
+        </SwiperSlide>
+      ))}
+      
+          
 
         </Swiper>
-        <button className={classes.view}>View All</button>
+        {!showAllProducts &&
+                    <button className={classes.view} onClick={toggleProductsView}>View More</button>
+                }
+
+
+        {showAllProducts && (
+                <div className={classes.allProducts}>
+                  
+                    <div className={classes.productList}>
+                        {products.map(product => (
+                            <div key={product.id} className={classes.productItem}>
+                                <Product cls={product.cls} gradientColors={product.gradientColors} />
+                                
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )}
+            
+                  { showAllProducts && <button className={classes.view} onClick={toggleProductsView}>View Less</button> }
+
 </div>
     );
 }
 
 export default ProductSlider
+
+
+
+// import React from 'react'
+// // Import Swiper React components
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import { Autoplay } from 'swiper/modules';
+// import 'swiper/css';
+// import { Pagination, Navigation } from 'swiper/modules';
+// import 'swiper/css/pagination';
+// import 'swiper/css/navigation';
+// import Product from './Product';
+// import Heading from '../Heading/Heading';
+// import classes from "./Product.module.css"
+// const ProductSlider = () => {
+//     const gradients = {
+//         p1: 'linear-gradient(180deg, #FCB48C 0%, #FFEADF 100%)',
+//         p2: 'linear-gradient(180deg, #6FDBF9 0%, rgba(207, 245, 255, 0.69) 100%)',
+//         p3: 'linear-gradient(180deg, #E8EB8C 0%, #FEFFEE 100%)',
+//         p4: 'linear-gradient(180deg, #FF9CA9 0%, rgba(255, 181, 191, 0.48) 100%)',
+//       };
+    
+//     return (
+//         <div  id='product'>
+//         <Heading heading="Our Products" cls={classes.div_head}/>
+//         <Swiper
+//         pagination={{
+//             type: 'progressbar',
+//           }}
+//           navigation={true}
+//           modules={[ Autoplay, Pagination, Navigation]}
+//             loop={true}
+//             autoplay={{
+//                 delay: 5000,
+//                 disableOnInteraction: true,
+//             }}
+//             className={'home_slider home_slider1 prmodal'}
+//             slidesPerView={1}
+//             breakpoints={{
+//                 360: {
+//                     slidesPerView: 1
+//                 },
+//                 690: {
+//                     slidesPerView: 2
+//                 },
+//                 1000: {
+//                     slidesPerView: 3
+//                 },
+//                 1200: {
+//                     slidesPerView: 4
+//                 }
+//             }}
+
+
+//             spaceBetween={30}
+//             // slidesPerView={4}
+//         >
+
+
+//       {[...Array(10)].map((_, index) => (
+//         <SwiperSlide key={index}>
+//           <Product cls={classes[`p${(index % 4) + 1}`]} gradientColors={gradients[`p${(index % 4) + 1}`]} />
+//         </SwiperSlide>
+//       ))}
+//             {/* <SwiperSlide><Product cls={classes.p1}/></SwiperSlide>
+//             <SwiperSlide><Product cls={classes.p2}/></SwiperSlide>
+//             <SwiperSlide><Product cls={classes.p3}/></SwiperSlide>
+//             <SwiperSlide><Product cls={classes.p4}/></SwiperSlide>
+//             <SwiperSlide><Product cls={classes.p1}/></SwiperSlide>
+//             <SwiperSlide><Product cls={classes.p2}/></SwiperSlide>
+//             <SwiperSlide><Product cls={classes.p3}/></SwiperSlide>
+//             <SwiperSlide><Product cls={classes.p4}/></SwiperSlide>
+//             <SwiperSlide><Product cls={classes.p1}/></SwiperSlide>
+//             <SwiperSlide><Product cls={classes.p2}/></SwiperSlide> */}
+          
+
+//         </Swiper>
+//         <button className={classes.view}>View All</button>
+// </div>
+//     );
+// }
+
+// export default ProductSlider
