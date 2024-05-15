448ddeaf4860e65479f5a307f9b5098790cea2a4
shriyatiwari17102000 <112067667+shriyatiwari17102000@users.noreply.github.com>
2024-05-15 12:26:35 +0530

    code commited

--- src/Components/Navbar/Home/Home.jsx
+++ src/Components/Navbar/Home/Home.jsx
@@ -46,7 +46,7 @@ const Home = () => {
                     slidesPerView: 1
                 },
                 1200: {
-                    slidesPerView: 1.5
+                    slidesPerView: 1.2
                 }
             }}
 
