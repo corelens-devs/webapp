c575695cd9ab44df055ee4653f7df97680424eb3
shriyatiwari17102000 <112067667+shriyatiwari17102000@users.noreply.github.com>
2024-05-21 13:03:30 +0530

    code commited

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -11,9 +11,10 @@ const Services = () => {
       <div id="services">
       <Heading heading={"What We Offer"} para="" cls={classes.div_head} />
     <div className={classes.service_div}>
-        <ServiceComp para={"Forget old CCTV CAMERAS and switch to our latest CAMERA-sensors which have alarms,mobile alerts,human detection alert to help stop theft when you're away from home. You can even book surveillance to watch your place while you're out at just 150rs per hour."} heading={"Camera sensors with on demand surveillance"} img={service1}/>
-        <ServiceComp para={"Introducing our sleek, lightweight and high-magnetic GPS trackers which you can hide anywhere in your CARS AND BIKES . We offer pinpoint-accurate GPS trackers with unbeatable battery life, geofencing, real-time alerts, apple and google maps support and much more.Easy to install, they’re a must-have for protecting your vehicles."} heading={"Gps trackers"} img={gps} cls={classes.p2}/>
-        <ServiceComp heading={"24/7 support and Free home-service"} para={"Enjoy 1 year of free home service with our products. Plus, we offer free delivery across India, 24/7 toll-free support, easy returns and replacements. We provide a dedicated complaint manager  until your issue is resolved. Your issues are our priority and we stay with you until they are fixed .We don’t ignore your problems and we’ll keep assisting you until your issue is resolved completely."} img={service3} cls={classes.p3}/>
+        <ServiceComp para={"Forget old CCTV cameras and switch to our latest camera sensors. They come with alarms, mobile alerts, and human detection to help stop theft when you're away. You can even book surveillance for just 150 Rs per hour to watch your place while you're out."} heading={"Camera Sensors with On-Demand Surveillance"} img={service1}/>
+        <ServiceComp para={"Our Slim ,lightweight and powerful magnetic GPS trackers can be hidden anywhere in your cars and bikes. They have long battery life, geofencing, real-time alerts, and support for Apple and Google Maps. Easy to install, they're essential for keeping your vehicles safe."} heading={"Gps Trackers"} img={gps} cls={classes.p2}/>
+
+        <ServiceComp heading={"24/7 Support and Free Home Service"} para={"Get 1 year of free home service with our products. We offer free delivery across India, 24/7 toll-free support, easy returns, and replacements. We provide a dedicated manager to resolve your issues completely. Your problems are our priority, and we’ll keep assisting you until they're fixed."} img={service3} cls={classes.p3}/>
  
     </div>
     </div>
