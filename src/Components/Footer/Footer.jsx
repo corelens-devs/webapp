75749236c7c3562261207e387e05087a569f905f
shriyatiwari17102000 <112067667+shriyatiwari17102000@users.noreply.github.com>
2024-07-03 15:26:37 +0530

    code pushed

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import {FaInstagram} from 'react-icons/fa'
+import React, { useState } from 'react'
+import {FaInstagram, FaPhone, FaPhoneAlt} from 'react-icons/fa'
 import { FiFacebook, FiGithub } from "react-icons/fi";
 import classes from "./Footer.module.css"
 import { FaXTwitter } from "react-icons/fa6";
 import {useNavigate} from "react-router-dom"
 import logo from "../../Assets/logo.png"
+import { IoLocationSharp } from "react-icons/io5";
+import { MdEmail } from "react-icons/md";
+
+
 const Footer = () => {
+  const [showPrivacyPolicy, setShowPrivacyPolicy] = useState(false);
+
+  const togglePrivacyPolicy = () => {
+      setShowPrivacyPolicy(prevState => !prevState);
+  };
   const navigate = useNavigate()
   const handleNavigation = (path) => {
     navigate(path);
@@ -25,8 +34,55 @@ const Footer = () => {
             <p style={{cursor:"pointer"}} onClick={() => handleNavigation('/term-sales')}>Terms of Sales</p>
             <p style={{cursor:"pointer"}} onClick={() => handleNavigation('/term-condition')}>Term & Condition</p>
             <p style={{cursor:"pointer"}} onClick={() => handleNavigation('/privacy-policy')}>Privacy Policy</p>
+            <p style={{cursor:"pointer"}} onClick={togglePrivacyPolicy}>Contact Us</p>
             <p style={{cursor:"pointer"}}>Download App</p>
         </div>
+        {showPrivacyPolicy && (
+                <div className={classes.contactinfo}>
+                <div className={classes.contactsection}>
+                    <h3>Get In Touch</h3>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+                <div className={classes.top_contact}>
+                <div className={classes.top_div}>
+           <div className={classes.icon}>
+           <FaPhoneAlt style={{fontSize:"18px"}} />
+
+           </div>
+
+                  <div className={classes.contact}>
+                  <p>Give us a call</p>
+                  <a href="tel:18003134207">1800-313-4207</a>
+                  </div>
+                </div>
+                <div className={classes.top_div}>
+                <div className={classes.icon}>
+                <MdEmail />
+                </div>
+
+                   <div  className={classes.contact}>
+                   <p>Send us an email</p>
+                    <a href="mailto:customercare@corelens.in">customercare@corelens.in</a>
+                    <br/>
+                    <a href="mailto:connect@corelens.in">connect@corelens.in</a>
+                   </div>
+                </div>
+                <div className={classes.top_div}>
+              <div className={classes.icon1}>
+              <IoLocationSharp />
+              </div>
+
+                  <div className={classes.contact}>
+                  <p>Visit us</p>
+                    <address>
+                        <span>VIBHOR ELECTRONICS</span><br />
+                        No 3, building 2287/39 MS ROAD, K. PUL, Delhi-110006
+                    </address>
+                  </div>
+                </div>
+                </div>
+            </div>
+            )}
         <div className={classes.div2}>
             <p>@Copyright2024,All Rights Reserved</p>
             <div>
