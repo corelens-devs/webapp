2c73f93e8c7b0322d15fe84f0cc45d076636e684
Shriya Tiwari <shriyatiwari87@gmail.com>
2024-11-05 17:55:58 +0530

    code added

--- src/Components/AISection/AISecurity.jsx
+++ src/Components/AISection/AISecurity.jsx
@@ -5,9 +5,10 @@ import Heading from '../Heading/Heading'
 import { HiOutlinePlayCircle } from "react-icons/hi2";
 import { TiVendorAndroid } from "react-icons/ti";
 import { ImAppleinc } from "react-icons/im";
+import { useNavigate } from 'react-router-dom';
 
 const AISecurity = () => {
-
+    const navigate = useNavigate()
     const downloadApk = () => {
         // On click, navigate to call this number
         window.location.href = "/corelens.apk";
@@ -21,8 +22,15 @@ const AISecurity = () => {
     return (
         <div id='Aihome' className={classes.ai_home}>
             <Heading cls={classes.mb} heading={'Want to secure your home and vehicles from theft?'} />
-            <button className={classes.btn1}><span>See Video</span> <HiOutlinePlayCircle />
-            </button>
+            <a
+                target="_blank"
+                className={classes.btn1}
+                href="https://www.youtube.com/watch?v=fSQbZqnMMaQ"
+                rel="noopener noreferrer"
+            >
+                <span>See Video</span>
+                <HiOutlinePlayCircle />
+            </a>
             <p className={classes.p}>Download corelens app now</p>
             <div className={classes.btm_btn_div}>
                 <button className={classes.btm_btn} onClick={downloadIos}><ImAppleinc style={{ fontSize: "30px" }} />
